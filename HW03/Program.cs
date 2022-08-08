e9887d11d211060d5ef46098d3e50e27e542c5af
Aleksey <teleccabaka@example.com>
2022-08-08 13:15:29 +0500

    написал программу и сххему

---
+++ HW03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine()) ;
+if(value%2==0)
+{
+Console.Write("Число: ");
+Console.Write(value);
+Console.Write(" чётное ");
+}
+else
+{
+Console.Write("Число: ");
+Console.Write(value);
+Console.Write(" не чётное ");
+}
