992963b7a8344611cdcc22128c7844a6474fd074
Aleksey <teleccabaka@example.com>
2022-08-08 08:16:21 +0500

    написал программу

--- HW01/Program.cs
+++ HW01/Program.cs
@@ -1,3 +1,22 @@
-﻿Console.WriteLine("Введите числа");
-int value = ();
-int value = ();
+﻿Console.WriteLine("Введите число a: ");
+int value = Convert.ToInt32(Console.ReadLine()) ;
+Console.WriteLine("Введите число b: ");
+int value2 = Convert.ToInt32(Console.ReadLine()) ;
+if (value<value2)
+{
+Console.Write("Число ");
+Console.Write(value);
+Console.Write(" минимальное, число  ");
+Console.Write(value2);
+Console.WriteLine(" максимальное . ");
+}
+else
+{
+  Console.Write("Число ");
+Console.Write(value2);
+Console.Write(" минимальное, число  ");
+Console.Write(value);
+Console.WriteLine(" максимальное . ");  
+}
+
+
