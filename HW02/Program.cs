ab1002564933d78026b992fdb39b178ea26dad0a
Aleksey <teleccabaka@example.com>
2022-08-08 10:16:53 +0500

    сделал вторую задачу и схему

---
+++ HW02/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число a: ");
+int value = Convert.ToInt32(Console.ReadLine()) ;
+Console.WriteLine("Введите число b: ");
+int value2 = Convert.ToInt32(Console.ReadLine()) ;
+Console.WriteLine("Введите число c: ");
+int value3 = Convert.ToInt32(Console.ReadLine()) ;
+
+int max = value;
+if(max<value2)
+{
+ if(value2>value3)
+ {
+    max=value2;
+ }
+ else{
+    max=value3;
+ }
+}
+else
+{
+   if (max < value3)
+   {
+    max=value3;
+   }
+   else{
+    max=max;
+   }
+}
+Console.Write("максимальное число : ");
+Console.Write(max);
