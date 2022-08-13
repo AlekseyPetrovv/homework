036e1245a4d5f36d89033dd7fccc464546237c5b
Aleksey <teleccabaka@example.com>
2022-08-13 19:09:55 +0500

    Hаписал программу

--- HomeWorkLection2/HW11/Program.cs
+++ HomeWorkLection2/HW11/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int M1(int value)
+{
+int a = value/10;
+int b = a%10;
+return(b);
+}
+int val = new Random().Next(100,1000);
+Console.WriteLine(val);
+int c =M1(val);
+Console.WriteLine(c);
+
+
